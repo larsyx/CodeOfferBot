925b4948d1557f9c1cec21a6481626e57edd4ed5
larsyx <gabriele.2000.02@gmail.com>
2023-08-12 16:06:48 +0200

    Implementazione ProductDialog

--- dialogs/ProductDialog.js
+++ dialogs/ProductDialog.js
@@ -1,17 +1,34 @@
-const { ComponentDialog, ChoicePrompt, WaterfallDialog } = require("botbuilder-dialogs");
+const { MessageFactory } = require("botbuilder-core");
+const { ComponentDialog, ChoicePrompt, WaterfallDialog, TextPrompt } = require("botbuilder-dialogs");
 const CHOICE_PROMPT = 'ChoicePrompt';
-const WATERFALL_DIALOG = 'WaterfallDialog';
+const MAIN_DIALOG = 'MainDialog';
 const PRODUCT_DIALOG = 'ProductDialog';
+const TEXT_PROMPT = 'TextPrompt';7
+const FIND_PRODUCT = 'FindProduct';
+const CATEGORY_PRODUCT = 'CategoryProduct';
+
+
 
 class ProductDialog extends ComponentDialog{
     constructor(userState){
         super(PRODUCT_DIALOG);
+        this.addDialog(new TextPrompt(TEXT_PROMPT));
         this.addDialog(new ChoicePrompt(CHOICE_PROMPT));
-        this.addDialog(new WaterfallDialog(WATERFALL_DIALOG,[
-            this.choiceStep.bind(this)
+        this.addDialog(new WaterfallDialog(MAIN_DIALOG,[
+            this.choiceStep.bind(this),
+            this.selectChoice.bind(this)
+        ]));
+        this.addDialog(new WaterfallDialog( FIND_PRODUCT, [
+            this.findProduct.bind(),
+            this.findAndShow.bind(),
         ]));
 
-        this.initialDialogId = WATERFALL_DIALOG;
+        this.addDialog(new WaterfallDialog( CATEGORY_PRODUCT, [
+            this.showCategory.bind(),
+            this.showProductByCategory.bind(),
+        ]));
+
+        this.initialDialogId = MAIN_DIALOG;
     }
 
     async run(turnContext, accessor) {
@@ -28,10 +45,51 @@ class ProductDialog extends ComponentDialog{
     async choiceStep(step){
         return await step.prompt(CHOICE_PROMPT, 
             'Seleziona un opzione:',
-            ['opzione1', 'opzione2', 'opzione3']
+            ['Più convenienti', 'Categorie', 'Suggerimenti', 'Cerca']
         );
-
     }
+
+    async selectChoice(step){
+        const choice = step.result.value;
+
+        switch(choice){
+            case 'Più convenienti':
+                await step.context.sendActivity(MessageFactory.text('sono in più convenienti'));
+                break;
+            case 'Categorie':
+                return await step.replaceDialog(FIND_PRODUCT);
+            case 'Suggerimenti':
+                await step.context.sendActivity(MessageFactory.text('sono in suggerimenti'));
+                break;
+            case 'Cerca':
+                return await step.replaceDialog(FIND_PRODUCT);
+            default:
+                await step.context.sendActivity(MessageFactory.text('Operazione non supportata riprova'));
+                return await step.replaceDialog(MAIN_DIALOG);
+        }
+
+        return await step.endDialog();
+    }
+
+    async findProduct(step){
+        return await step.prompt(TEXT_PROMPT, 'Cosa stai cercando?');
+    }
+
+     async findAndShow(step){
+        const result = step.result;
+        await step.context.sendActivity(MessageFactory.text('Stai cercando: ' + result));
+        return await step.next;
+     }
+
+     async showCategory(step){
+
+     }
+
+     async showProductByCategory(step){
+
+     }
+
+
 }
 
 module.exports.ProductDialog = ProductDialog;
